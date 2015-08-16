abc67d817194759fd77f86e008d3051bc758fc3a
Shadab Zafar <dufferzafar0@gmail.com>
2015-08-16 20:43:40 +0530

    Router recievePacket function

--- Router.java
+++ Router.java
@@ -38,4 +38,24 @@ public class Router {
             case C: tableC.add(destinationIP, nextHop, iface); break;
         }
     }
+
+    public void receivePacket(Packet packet) {
+        System.out.println("Recieving Packet: ");
+        System.out.println("-----------------\n");
+
+        Table table = new Table();
+        switch (packet.getIPClass()) {
+            case A: table = tableA; break;
+            case B: table = tableB; break;
+            case C: table = tableC; break;
+            case Invalid: System.out.println("Packet Discarded!"); return;
+        }
+
+        Table.Entry entry = table.findDestinationIP(packet.getDestinationIP().getNetAddress());
+        if (entry.iface == 0) {
+            System.out.println("Network IP not found. Route to default IP.");
+        } else {
+            System.out.printf("Route packet to: %s through interface: %d", entry.nextHop, entry.iface);
+        }
+    }
 }
